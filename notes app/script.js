98a7aa4d0bdeedeb537e39ec1521814b188c92f7
monika39-maker <dangolmonika@yahoo.com>
2021-07-07 11:21:22 +0100

    added new function in notes app-adding new note when add button in clicked

--- notes app/script.js
+++ notes app/script.js
@@ -1,8 +1,37 @@
 const editBtn= document.getElementById('edit');
-const deleteBtn= document.getElementById('delete');
-const textArea= document.getElementById('text-area');
-const addBtn = document.getElementById('add')
 
-editBtn.addEventListener("click", () => {
-    marked(textArea.value)
-})
+const textArea= document.getElementById('text-area');
+const addBtn = document.getElementById('add');
+const editArea = document.getElementById('edit-area');
+const mainEl = document.getElementById('main')
+
+// editBtn.addEventListener("input", (e) => {
+//     textArea.classList.toggle('hidden');
+//     editArea.classList.toggle('hidden')
+    
+// })
+
+addBtn.addEventListener("click", () => {
+    
+    addNewNote()
+    
+});
+
+function addNewNote() {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('container')
+    mainEl.innerHTML = `<div class="tools">
+            <button id="edit"><i class="fas fa-edit"></i></button>
+            <button id="delete" class="delete"><i class="fas fa-trash"></i></button>
+        </div>
+        <div class="text hidden" id="edit-area"></div>
+        <textarea name="" id="text-area" cols="30" rows="10"></textarea>`
+
+    mainEl.appendChild(newDiv);
+    const deleteBtn = newDiv.querySelector('.delete');
+    deleteBtn.addEventListener("click", () => {
+        alert("alert")
+        
+    })
+}
+
