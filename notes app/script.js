f0b0baef8bcca8945a3df3c7b2b3550e491ffc6b
monika39-maker <dangolmonika@yahoo.com>
2021-07-12 11:43:05 +0100

    notes app - added adding notes function

--- notes app/script.js
+++ notes app/script.js
@@ -1,19 +1,21 @@
 const addBtn = document.getElementById('add');
 
-const mainEl = document.querySelector('.main')
+
+
+const mainEl = document.querySelector('.main');
 
 
 
 addBtn.addEventListener("click", () => {
-    
-    addNewNote()
+    addNewNote();
     
 });
 
 function addNewNote() {
+    
     const newDiv = document.createElement('div');
     newDiv.classList.add('container')
-    mainEl.innerHTML = `<div class="tools">
+    newDiv.innerHTML = `<div class="tools">
             <button class="edit"><i class="fas fa-edit"></i></button>
             <button class="delete"><i class="fas fa-trash"></i></button>
         </div>
@@ -22,27 +24,27 @@ function addNewNote() {
 
     mainEl.appendChild(newDiv);
     
-    const textarea = mainEl.querySelector('.text-area');
-    const editArea = mainEl.querySelector('.edit-area')
-    const editBtn = mainEl.querySelector('.edit');
-    const removeBtn = mainEl.querySelector('.delete')
+    const textarea = newDiv.querySelector('.text-area');
+    const editArea = newDiv.querySelector('.edit-area')
+    const editBtn = newDiv.querySelector('.edit');
+    const removeBtn = newDiv.querySelector('.delete')
     editBtn.addEventListener("click", () => {
         textarea.classList.toggle('hidden');
         editArea.classList.toggle('hidden')
     })
     
     removeBtn.addEventListener("click", () => {
-        mainEl.remove()
+        newDiv.remove()
     })
 
     editArea.addEventListener("input", (e) => {
         const {value} = e.target;
         textarea.innerHTML = marked(value)
-        console.log(value)
+        
 
     })
 
-
+    
 
 }
 
