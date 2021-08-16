2af86c1500cd6321dc3b241fa5702e9f81a4b056
chayashital <chayadangol@gmail.com>
2021-08-16 14:31:28 +0100

    started drawing app

---
+++ drawing app/script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d')
+
+canvas.addEventListener('mousedown', () => {
+    alert("hello")
+})
+
+function draw(x, y) {
+    ctx.getArc(x, y, size,)
+}
