5cca70e7e50b611cebdd8caf6ad5bc03e2917729
chayashital <chayadangol@gmail.com>
2021-08-17 15:51:39 +0100

    added circle in canvas

--- drawing app/script.js
+++ drawing app/script.js
@@ -1,11 +1,23 @@
 const canvas = document.getElementById('canvas');
-
+const sizeText = document.getElementById('size-text');
+const plusBtn = document.getElementById('plus-btn')
+const minusBtn = document.getElementById('minus-btn')
 const ctx = canvas.getContext('2d')
 
-canvas.addEventListener('mousedown', () => {
-    alert("hello")
+canvas.addEventListener('mousedown', (e) => {
+    const x = e.offsetX
+    const y = e.offsetY
+    draw(x, y)
 })
+var size = 30
 
+sizeText.innerText = size
 function draw(x, y) {
-    ctx.getArc(x, y, size,)
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, 2*Math.PI);
+    ctx.stroke()
 }
+
+plusBtn.addEventListener('click', () => {
+    size =+ 5
+})
