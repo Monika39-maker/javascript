932aaaeeecb14590e54f33b3dd23273ad56e4859
monika39-maker <dangolmonika@yahoo.com>
2021-08-21 12:49:53 +0100

    added the color changing porperty to drawing app canva circle

--- drawing app/script.js
+++ drawing app/script.js
@@ -7,39 +7,50 @@ const ctx = canvas.getContext('2d')
 
 
 var isPressed = false;
-canvas.addEventListener('mousedown', () => {
-    isPressed = true
-})
+
+canvas.addEventListener('mousemove', (e) => {
+    canvas.addEventListener('mousedown', (e) => {
+        isPressed = true
+        const x1 = e.offsetX
+        const y1 = e.offsetY
+        console.log(x1, y1)
+    })
 
 canvas.addEventListener('mouseup', () => {
-    isPressed=false
-})
-canvas.addEventListener('mousemove', (e) => {
+        isPressed=false;
+        const x2 = undefined
+        const y2 = undefined
+    });
+
     if (isPressed) {
-        const x = e.offsetX
-        const y = e.offsetY
-        draw(x, y)
+        x2 = e.offsetX;
+        y2 = e.offsetY;
+        console.log(x2, y2)
+        x1 = x2;
+        y1 = y2;
+        drawLine(x1, y1, x2, y2);
     }
+    
 
 })
 var size = 10
-var color = input.target.value;
-console.log(color)
 
 
-function draw(x, y) {
+
+
+
+function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2*Math.PI);
+    ctx.moveTo(x1, y1, size, 0, 2*Math.PI);
+    ctx.lineTo(x2, y2, size, 0, 2*Math.PI);
+    input.addEventListener('change', (e) => {
+        ctx.strokeStyle = e.target.value;
+
+    })
+    
     ctx.stroke();
-    ctx.strokeStyle = color;
 };
 
-function line(x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2*Math.PI);
-    ctx.stroke();
-    ctx.strokeStyle = color;
-}
 
 plusBtn.addEventListener('click', () => {
     size = size+5;
