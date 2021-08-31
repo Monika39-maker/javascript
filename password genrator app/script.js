05abfc904bc76eec5f8cb96eebac3ab0472a4076
chayashital <chayadangol@gmail.com>
2021-08-31 12:53:10 +0100

    half way to password generator app

--- password genrator app/script.js
+++ password genrator app/script.js
@@ -1,7 +1,9 @@
 const lengthEl  = document.getElementById('length-input')
 const upperCaseEl  = document.getElementById('upperCase-input')
-const lowerCaseEl  = document.getElementById('lowerCase-inut')
-const charEl = document.getElementById('char-input')
+const lowerCaseEl  = document.getElementById('lowerCase-input');
+const numberEl = document.getElementById('number-input');
+const charEl = document.getElementById('char-input');
+const submitEl = document.getElementById('submit')
 
 const upperCase  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const lowerCase  = 'abcdefghijklmnopqrstuvwxyz'
@@ -22,13 +24,39 @@ function createRandomUpperCase() {
 function createRandomLowerCase() {
     return Math.floor(Math.random() * lowerCase.length)
 }
+function createRandomNumber() {
+    return Math.floor(Math.random() * number.length)
+}
 function createRandomChar() {
     return Math.floor(Math.random() * char.length)
 }
- 
-function lengthOfPassword() {
-    const passwordLength = lengthEl.value;
-    console.log(passwordLength) 
+
+
+submitEl.addEventListener('click', generatePassword)
+
+function generatePassword() {
+    let len = lengthEl.value;
+    let pw = ''
+    for (let i=0; i<len; i++) {
+        if (upperCaseEl.checked) {
+            const upperLetter = createRandomUpperCase()
+            pw += upperLetter
+        }
+        if (lowerCaseEl.checked) {
+            const lowerLetter = createRandomLowerCase()
+            pw += lowerLetter
+        }
+        if (numberEl.checked) {
+            const num = createRandomNumber()
+            pw += num
+        }
+        if (charEl.checked) {
+            const char = createRandomChar()
+            pw += char
+        }
+        alert(pw)
+    }
 
 }
-lengthOfPassword()
+    
+
